e46eca2c77fdcb244b83415287947143c9501466
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-11-02 23:19:28 +0200

    formatted strings

--- basic_algo/Strings.py
+++ basic_algo/Strings.py
@@ -9,3 +9,11 @@ print("Prints from the end: "+course[-2])
 #slicing
 print(course[0:3])
 print(course[:]) # prints the whole string
+
+#application of formatted strings
+first = 'Manzi'
+last= 'Cedro'
+
+#The Example: You are by the names of [Manzi][Cedrick]
+msg = f'You are by the names of [{first}][{last}]'
+print(msg)
