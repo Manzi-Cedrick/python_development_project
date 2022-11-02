a8257596210138ee1aea3496af51f9bda6c299d6
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-11-02 23:06:31 +0200

    type converstion from input data

---
+++ basic_algo/typeconversion.py
@@ -0,0 +1,5 @@
+birth_year = input("The Birth year: ");
+#Whenever you use the input string function you input a string value
+#So you need to convert it to an integer
+age = 2022 - int(birth_year)
+print(age)
