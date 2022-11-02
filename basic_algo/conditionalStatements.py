ff04ad687e90f35ff7feccf1922318b3c26db875
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-11-02 23:28:51 +0200

    The If statement controlls

---
+++ basic_algo/conditionalStatements.py
@@ -0,0 +1,8 @@
+is_hot = False
+is_cold = True
+if is_hot:
+    print("The weather outside is too hot.")
+elif is_cold:
+    print("The weather outside is too cold.")
+else:
+    print("The weather outside is not too hot.")
