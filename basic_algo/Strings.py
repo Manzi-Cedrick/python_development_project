253af36a5ae2a96d0ce26218a2eb0ac57b68bd62
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-11-02 23:09:44 +0200

    slicing strings

--- basic_algo/Strings.py
+++ basic_algo/Strings.py
@@ -2,3 +2,10 @@
 course = 'Python for "beginners" and the courses are "python" and "django"'
 print(course)
 
+#square bracket syntax
+print(course[0])
+print("Prints from the end: "+course[-2])
+
+#slicing
+print(course[0:3])
+print(course[:]) # prints the whole string
